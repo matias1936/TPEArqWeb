ad7f08b67f5c50f2b2f44503b4f34fd27c90a0b0
matias1936 <matiastoledo1936@gmail.com>
2026-08-30 17:36:24 -0300

    Creación de entidades.

--- src/main/java/entities/Factura.java
+++ src/main/java/entities/Factura.java
@@ -1,4 +1,40 @@
 package entities;
 
 public class Factura {
+
+    private Integer idFactura;
+    private Integer idCliente;
+
+    public Factura(Integer idFactura, Integer idCliente) {
+        this.idFactura = idFactura;
+        this.idCliente = idCliente;
+    }
+
+    public Factura() {
+
+    }
+
+    public Integer getIdFactura() {
+        return idFactura;
+    }
+
+    public void setIdFactura(Integer idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Factura{" +
+                "idFactura=" + idFactura +
+                ", idCliente=" + idCliente +
+                '}';
+    }
 }
