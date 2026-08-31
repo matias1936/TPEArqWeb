49d089f5563f8490d1803e121193fe18193df7ac
lucia adela ferraresi <luciaadelaferraresi@MacBook-Pro-de-lucia.local>
2026-08-31 16:03:59 -0300

    arme la conexion

--- src/main/java/repository/mysql/MySQLConnectionManager.java
+++ src/main/java/repository/mysql/MySQLConnectionManager.java
@@ -1,4 +1,55 @@
 package repository.mysql;
-
-public class MySQLConnectionManager {
+package factory.ConnectionManager;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class MySQLConnectionManager implements ConnectionManager {
+    private static volatile MySQLConnectionManager instance;
+    private Connection connection;
+    private static final String URL = "jdbc:mysql://localhost:3306/demodao";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    private MySQLConnectionManager() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection(
+                    URL,
+                    USER,
+                    PASSWORD
+            );
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    } 
+    public static MySQLConnectionManager getInstance() {
+        if (instance == null) {
+            synchronized (MySQLConnectionManager.class) {
+                if (instance == null) {
+                    instance = new MySQLConnectionManager();
+                }
+            }
+        }
+        return instance;
+    }
+    @Override
+    public Connection getConnection() {
+        return connection;
+    }
+    @Override
+    public void shutdown() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            connection = null;
+            synchronized (MySQLConnectionManager.class) {
+                instance = null;
+            }
+        }
+    }
 }
